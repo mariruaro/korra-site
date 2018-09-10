8cfa3db50f01f8a641fa59b1f93a4c29363b3305
marianavigano <mariana@fdd.com.br>
2018-09-09 22:58:00 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		css3:true,
 		scrollingSpeed: 1000,
-		anchors: ['home', 'history', '3rdPage'],
+		anchors: ['home', 'history', 'carcters', 'seasons'],
 		onLeave: function(index, newIndex, direction){
 			console.log(index-1, newIndex-1, direction);
 			$('#fullpage section').eq(newIndex-1).addClass('move');
