94a8bd8bfb98d9db8af944f05f7d042324be4349
Mariana Ruaro Viganó <34455544+mariruaro@users.noreply.github.com>
2018-09-13 20:30:05 -0300

    js-file

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		css3:true,
 		scrollingSpeed: 1000,
-		anchors: ['home', 'history', 'carcters', 'seasons'],
+		anchors: ['home', 'history', 'carcters', 'seasons', 'curiosities'],
 		onLeave: function(index, newIndex, direction){
 			console.log(index-1, newIndex-1, direction);
 			$('#fullpage section').eq(newIndex-1).addClass('move');
